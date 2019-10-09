9e818841d18ae2e54a8624733c8888bf8abbcd32
Swarup Dey Sarkar <deysarkarswarup123@gmail.com>
2019-10-10 04:31:54 +0530

    Update emotion_recognition.py

--- emotion_recognition.py
+++ emotion_recognition.py
@@ -49,38 +49,39 @@ X_train = X_train.reshape(X_train.shape[0], 48, 48, 1)
 
 X_test = X_test.reshape(X_test.shape[0], 48, 48, 1)
 
-##designing the cnn
+##designing the cnn in keras
 #1st convolution layer
 model = Sequential()
 
 model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', input_shape=(X_train.shape[1:])))
 model.add(Conv2D(64,kernel_size= (3, 3), activation='relu'))
 # model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2,2), strides=(2, 2)))
-model.add(Dropout(0.5))
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2, 2)))#picks up max value 
+model.add(Dropout(0.5))#to stop overfitting
 
 #2nd convolution layer
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(2*64, (3, 3), activation='relu'))
+model.add(Conv2D(2*64, (3, 3), activation='relu'))
 # model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2,2), strides=(2, 2)))
-model.add(Dropout(0.5))
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2, 2)))#picks up max value 
+model.add(Dropout(0.5))#to stop overfitting
 
 #3rd convolution layer
-model.add(Conv2D(128, (3, 3), activation='relu'))
-model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Conv2D(2*2*64, (3, 3), activation='relu'))
+model.add(Conv2D(2*2*64, (3, 3), activation='relu'))
 # model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2,2), strides=(2, 2)))
 
-model.add(Flatten())
+model.add(Flatten())#transforming the pool into a single column
 
 #fully connected neural networks
-model.add(Dense(1024, activation='relu'))
+model.add(Dense(2*2*64, activation='relu'))
 model.add(Dropout(0.2))
-model.add(Dense(1024, activation='relu'))
+model.add(Dense(2*64, activation='relu'))
 model.add(Dropout(0.2))
 
 model.add(Dense(num_labels, activation='softmax'))
+#model.summary()
 
 #Compliling the model
 model.compile(loss=categorical_crossentropy, optimizer=Adam(), metrics=['accuracy'])
